923ee4abbfdb70773924d48baf6a2554b8baf2e0
AxdaSilva <dasilvaalex99@gmail.com>
2025-11-24 10:13:40 +0100

    modal meditacion

---
+++ backend/src/main/java/com/nexe/backend/model/Medication.java
@@ -0,0 +1,15 @@
+package com.nexe.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "medications")
+public class Medication {
+
+
+}
