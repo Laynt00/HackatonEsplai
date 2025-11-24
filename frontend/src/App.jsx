5e8a182521eb27455bab48837a30f85904a0f9e4
Alvaro-pc-Ra <laynt00@gmail.com>
2025-11-24 15:09:57 +0100

    feat: add page transitions and integrate framer-motion for animated routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
 import "./App.css";
 import Comunication from "./pages/Comunication/Comunication";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Peques from "./pages/Peques/Peques";
 import Home from "./pages/Home/Home";
 import Login from "./components/Login/Login";
+import { AnimatePresence } from "framer-motion";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/peques" element={<Peques />} />
+        <Route path="/comunication" element={<Comunication />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login /> } />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/peques" element={<Peques />} />
-          <Route path="/comunication" element={<Comunication />} />
-        </Routes>
+        <AnimatedRoutes />
       </BrowserRouter>
     </>
   );
