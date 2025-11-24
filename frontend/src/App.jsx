beb6a2fe71b10a544482708a4e2d0e7fa83be449
pedro <pedropinedocobo@hotmail.com>
2025-11-24 10:18:01 +0100

    creación del calendario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Dashboard } from './pages/Dashboard/Dashboard'
 
 function App() {
 
   return (
     <>
-      <h1>PROYECTO LIMPIO</h1>
+      <Dashboard />
     </>
   )
 }
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import "./DashBoard.css";
+//import { Header } from "../../components/Header/Header";
+import { Calendar } from "../../components/Calendar/Calendar";
+import { useState, useEffect, useContext } from "react";
+import { ListaPacientesDashboard } from "../../components/ListaPacientesDashboard/ListaPacientesDashboard";
 
 export const Dashboard = () => {
+  const [selectedDay, setSelectedDay] = useState(new Date());
+  const [appointments, setAppointments] = useState([]);
+  //El token hay que traerselo del contexto
+  const [token, setToken] = useState("sdjndfuidf")
+  
+  const formatDate = (d) => d?.toISOString().split("T")[0] || "";
+
+	useEffect(() => {
+	if (!selectedDay) return;
+        const fetchAppointments = async () => {
+            try {
+                const res = await fetch(
+                    `http://localhost:8080/schedule/date/${formatDate(selectedDay)}`,
+                    { headers: { Authorization: `Bearer ${token}` } }
+                );
+                if (!res.ok) throw new Error("Error al cargar citas");
+                const data = await res.json();
+                setAppointments(data);
+            } catch (e) {
+                console.error(e);
+                setAppointments([]);
+            }
+        };
+	fetchAppointments();
+	}, [selectedDay, token]);
+
+
   return (
-    <section className='DashboardPage'>
-        
-    </section>
-  )
-}
+    <div className="DashboardPage">
+      
+      <div className="DashboardPage__content">
+        <Calendar
+          selectedDay={selectedDay}
+          setSelectedDay={setSelectedDay}
+          appointments={appointments} 
+        />
+        <ListaPacientesDashboard
+          appointments={appointments} 
+        />
+      </div>
+    </div>
+  );
+};
