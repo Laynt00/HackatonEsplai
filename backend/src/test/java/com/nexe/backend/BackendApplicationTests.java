09c82f4befeb0f078896d3a53f3256a8489675ad
Ana <anapaulaquinoa@gmail.com>
2025-11-24 09:38:23 +0100

    first commit

---
+++ backend/src/test/java/com/nexe/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
